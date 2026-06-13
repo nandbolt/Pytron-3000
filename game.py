43cd2a46dbabc7dfb61d8c3e86b3621db0b7f7df
nandbolt <alexwilson5528@gmail.com>
2026-06-13 17:18:06 -0500

    image

--- game.py
+++ game.py
@@ -1,19 +1,45 @@
 import sys
 import pygame
 
-def main():
-    pygame.init()
-    pygame.display.set_caption('Pytron3000')
-    screen = pygame.display.set_mode((640, 480))
-    clock = pygame.time.Clock()
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
+class Game:
+    def __init__(self):
+        pygame.init()
+        pygame.display.set_caption('Pytron3000')
+        self.screen = pygame.display.set_mode((640, 480))
+        self.clock = pygame.time.Clock()
 
-        pygame.display.update()
-        clock.tick(60)
+        self.img = pygame.image.load('data/images/pytron/pytron.png')
+        self.img.set_colorkey((0, 0, 0))
+        self.img_pos = [160, 260]
+        self.movement = [False, False]
+    
+    def run(self):
+        while True:
+            self.screen.fill((0, 0, 0))
+            self.img_pos[1] += self.movement[1] - self.movement[0]
+            self.screen.blit(self.img, self.img_pos)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_w:
+                        self.movement[0] = True
+                    if event.key == pygame.K_s:
+                        self.movement[1] = True
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_w:
+                        self.movement[0] = False
+                    if event.key == pygame.K_s:
+                        self.movement[1] = False
+
+
+            pygame.display.update()
+            self.clock.tick(60)
+
+def main():
+    Game().run()
 
 if __name__ == '__main__':
     main()
